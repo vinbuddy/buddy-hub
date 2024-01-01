e97f9fe8968ce8e856fc6520ceef1267457a7f16
Vinh Huynh <huynhthevinh1608@gmail.com>
2024-01-01 23:06:36 +0700

    fix: middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,22 +3,26 @@ import { NextRequest } from "next/server";
 import { authMiddleware } from "@clerk/nextjs";
 
 const protectedRoutes = ["/", "/video", "/explore"];
-let isAuthenticated = true; // fake auth
+let isAuthenticated = false; // fake auth
 
-function protectedRouteMiddleware(req: NextRequest) {
-    // protected routes -> redirect login
-    if (!isAuthenticated && protectedRoutes.includes(req.nextUrl.pathname)) {
-        const absoluteURL = new URL("/login", req.nextUrl.origin);
-        return NextResponse.redirect(absoluteURL.toString());
-    }
-}
+// async function protectedRouteMiddleware(req: NextRequest) {
+//     // protected routes -> redirect login
+//     if (!isAuthenticated && protectedRoutes.includes(req.nextUrl.pathname)) {
+//         const absoluteURL = new URL("/login", req.nextUrl.origin);
+//         return NextResponse.redirect(absoluteURL.toString());
+//     }
+// }
 
-export default function middleware(req: NextRequest) {
-    protectedRouteMiddleware(req);
-    authMiddleware({
-        publicRoutes: ["/api/webhooks(.*)"],
-    });
-}
+export default authMiddleware({
+    publicRoutes: ["/api/webhooks(.*)"],
+});
+
+// export default async function middleware(req: NextRequest) {
+//     await protectedRouteMiddleware(req);
+//     authMiddleware({
+//         publicRoutes: ["/api/webhooks(.*)"],
+//     });
+// }
 
 export const config = {
     matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
