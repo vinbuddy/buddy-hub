9b603469f372e2126038ba0c624b18c1eca2a467
Vinh Huynh <huynhthevinh1608@gmail.com>
2024-01-01 23:20:57 +0700

    fix: authMiddleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,28 +1,9 @@
-import { NextResponse } from "next/server";
-import { NextRequest } from "next/server";
 import { authMiddleware } from "@clerk/nextjs";
-
-const protectedRoutes = ["/", "/video", "/explore"];
-let isAuthenticated = false; // fake auth
-
-// async function protectedRouteMiddleware(req: NextRequest) {
-//     // protected routes -> redirect login
-//     if (!isAuthenticated && protectedRoutes.includes(req.nextUrl.pathname)) {
-//         const absoluteURL = new URL("/login", req.nextUrl.origin);
-//         return NextResponse.redirect(absoluteURL.toString());
-//     }
-// }
 
 export default authMiddleware({
     publicRoutes: ["/api/webhooks(.*)"],
+    debug: true,
 });
-
-// export default async function middleware(req: NextRequest) {
-//     await protectedRouteMiddleware(req);
-//     authMiddleware({
-//         publicRoutes: ["/api/webhooks(.*)"],
-//     });
-// }
 
 export const config = {
     matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
